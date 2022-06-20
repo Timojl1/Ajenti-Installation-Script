0e262d90fce04cff86be860f766fc105fa5cb160
Timo <77027176+Timojl1@users.noreply.github.com>
2022-06-20 20:35:14 +0200

    Add files via upload

---
+++ Ajenti-Installer.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt -y upgrade
+wget https://launchpad.net/ubuntu/+source/pillow/4.1.1-3build2/+build/13243580/+files/python-imaging_4.1.1-3build2_all.deb
+sudo apt update
+sudo apt install python-olefile python-pil
+sudo dpkg -i python-imaging_4.1.1-3build2_all.deb
+sudo apt -y install gnupg2
+wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
+echo "deb http://repo.ajenti.org/debian main main debian" | sudo tee /etc/apt/sources.list.d/ajenti.list
+sudo apt update
+sudo apt install ajenti
+systemctl status ajenti
+sudo systemctl enable ajenti
+echo Ajenti will listen on HTTPS port 8000 by default
+echo Default username : root
+echo Default password : admin
